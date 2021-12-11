d453f4d64bb15ed647a6cec11512e83e2f8df848
Winzen <94500122+Winzen@users.noreply.github.com>
2021-12-11 15:30:46 -0300

    Update Problem 1 Multiples of 3 and 5.py

--- Project Euler/Problem 1 Multiples of 3 and 5.py
+++ Project Euler/Problem 1 Multiples of 3 and 5.py
@@ -1,16 +1,15 @@
+#LINK PROBLEM: https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-1-multiples-of-3-and-5
 
 """
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
 """
+import numpy as np
 
 def multiplesOf3and5(number):
-    soma = 0
-    for x in range(1, number):
-        if x % 3 == 0 or x % 5 == 0:
-            soma += x
-    return soma
-
+    sequencia = np.arange(1, number+1)
+    sequencia = sequencia[(sequencia % 3 == 0) | (sequencia % 5 == 0)].sum(dtype=int)
+    return sequencia
 
 if __name__ == '__main__':
     """should return a number."""
@@ -27,4 +26,3 @@ if __name__ == '__main__':
 
     """should return 89301183."""
     print(multiplesOf3and5(19564))
-
