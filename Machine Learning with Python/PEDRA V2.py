a541a7c198d0de08d5bac52454cc924a62b85298
Winzen <94500122+Winzen@users.noreply.github.com>
2021-12-22 23:47:02 -0300

    Update PEDRA V2.py

--- Machine Learning with Python/PEDRA V2.py
+++ Machine Learning with Python/PEDRA V2.py
@@ -1,4 +1,4 @@
-def player(prev_play, opponent_history=[], plb = ['S', 'R', 'S', 'R'], hist = [[] ,[] ,[]], jogadas_hist = []):
+def player(prev_play, opponent_history=[], plb = ['S', 'R', 'S', 'R'], hist = [[] ,[] ,[], []], jogadas_hist = []):
     opponent_history.append(prev_play if prev_play != '' else "R")
     chance_strage = [0, 0, 0, 0]
     ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
@@ -11,11 +11,12 @@ def player(prev_play, opponent_history=[], plb = ['S', 'R', 'S', 'R'], hist = [[
       for e in range(len(hist)):
         acertos = 0
         playlast10 = hist[e][-10:].copy()
-        for x in range(len(playlast10[-10:])):
+        for x in range(len(playlast10)):
           #print(e, ideal_response[opponentlast10[x]], playlast10[x])
           if ideal_response[opponentlast10[x]] == playlast10[x]:
             acertos += 1
-        chance_strage[e] = acertos/len(playlast10) if acertos != 0 else 0  
+        chance_strage[e] = acertos/len(playlast10) if acertos != 0 else 0
+     # print(chance_strage)  
         #
       #quincy
         
@@ -25,6 +26,7 @@ def player(prev_play, opponent_history=[], plb = ['S', 'R', 'S', 'R'], hist = [[
       elif len(set(opponent_history[-4:])) == 1:
         plb[0] = 'P' if opponent_history[-2] == 'P' else "R"  
       hist[0].append(plb[0])
+      #quincy
 
       #mrugesh
       plb[1] = ideal_response[ideal_response[max(set(jogadas_hist), key=jogadas_hist.count)]]
@@ -35,7 +37,14 @@ def player(prev_play, opponent_history=[], plb = ['S', 'R', 'S', 'R'], hist = [[
       plb[2] = ideal_response[ideal_response[jogadas_hist[-1]]]
       hist[2].append(plb[2])
       #print('kris')
-      
+
+      #abbey
+      plb[3] = ideal_response[max(set(opponent_history), key=opponent_history.count)]
+      plb[3] = ideal_response[plb[3]] if chance_strage[3] >= 0.6 else plb[3]
+      plb[3] = ideal_response[ideal_response[plb[3]]] if chance_strage[3] >= 0.5 and opponent_history[-1] == jogadas_hist[-1] else plb[3]
+      hist[3].append(plb[3])
+      #abbey
+      #print(plb)
       guess = plb[chance_strage.index(max(chance_strage))]
     
     jogadas_hist.append(guess)    
