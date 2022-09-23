018c160f91f5a9996977269eb50c6c618fc413bf
Luiz Eduardo <94500122+Winzen@users.noreply.github.com>
2022-09-23 05:00:00 -0300

    Update arithmetic_arranger.py

--- Scientific Computing with Python/arithmetic_arranger.py
+++ Scientific Computing with Python/arithmetic_arranger.py
@@ -2,8 +2,9 @@ def arithmetic_arranger(lista, mostra_resultado=False):
     if len(lista) > 5:
         return "Error: Too many problems."
 
-    gerenciador_Linhas = dict()
-    gerenciador_Linhas["Linha1"] = gerenciador_Linhas["Linhas2"] = gerenciador_Linhas["separador"] = gerenciador_Linhas["resultado"] = str()
+    gerenciador_linhas = dict()
+    gerenciador_linhas["Linha1"] = gerenciador_linhas["Linhas2"] = \
+        gerenciador_linhas["separador"] = gerenciador_linhas["resultado"] = str()
 
     for conta in lista:
 
@@ -16,34 +17,33 @@ def arithmetic_arranger(lista, mostra_resultado=False):
 
         y = conta.split(f"{opetador}")
 
-        if y[0].strip().isnumeric() == False or y[1].strip().isnumeric() == False:
+        if y[0].strip().isnumeric() is False or y[1].strip().isnumeric() is False:
             return "Error: Numbers must only contain digits."
         elif len(y[0].strip()) > 4 or len(y[1].strip()) > 4:
             return "Error: Numbers cannot be more than four digits."
 
         largura_problema = max(len(y[0].strip()), len(y[1].strip()))+2
-        gerenciador_Linhas["Linha1"] += f"{y[0].strip().rjust(largura_problema)}{' ' * 4 if conta != lista[-1] else ''}"
-        gerenciador_Linhas["Linhas2"] += f"{opetador}{y[1].strip().rjust(largura_problema-1)}{' ' * 4 if conta != lista[-1] else ''}"
-        gerenciador_Linhas["separador"] += f"{'-' * largura_problema}{' ' * 4 if conta != lista[-1] else ''}"
+        gerenciador_linhas["Linha1"] += f"{y[0].strip().rjust(largura_problema)}{' ' * 4 if conta != lista[-1] else ''}"
+        gerenciador_linhas["Linhas2"] += f"{opetador}{y[1].strip().rjust(largura_problema-1)}" \
+                                         f"{' ' * 4 if conta != lista[-1] else ''}"
+        gerenciador_linhas["separador"] += f"{'-' * largura_problema}{' ' * 4 if conta != lista[-1] else ''}"
 
-        if mostra_resultado == True:
+        if mostra_resultado:
             resultado = f"{int(y[0]) + int(y[1]) if opetador == '+' else int(y[0]) - int(y[1])}"
-            gerenciador_Linhas["resultado"] += f"{resultado.strip().rjust(largura_problema)}{' ' * 4 if conta != lista[-1] else ''}"
+            gerenciador_linhas["resultado"] += f"{resultado.strip().rjust(largura_problema)}" \
+                                               f"{' ' * 4 if conta != lista[-1] else ''}"
 
-    if mostra_resultado == True:
-        resultado = gerenciador_Linhas["Linha1"] + "\n" + gerenciador_Linhas["Linhas2"] + "\n" + gerenciador_Linhas[
-            "separador"]+"\n" + gerenciador_Linhas[
+    if mostra_resultado:
+        resultado = gerenciador_linhas["Linha1"] + "\n" + gerenciador_linhas["Linhas2"] + "\n" + gerenciador_linhas[
+            "separador"]+"\n" + gerenciador_linhas[
             "resultado"]
     else:
-        resultado = gerenciador_Linhas["Linha1"]+"\n"+gerenciador_Linhas["Linhas2"]+"\n"+gerenciador_Linhas["separador"]
+        resultado = gerenciador_linhas["Linha1"]+"\n"+gerenciador_linhas["Linhas2"]+"\n"+gerenciador_linhas["separador"]
 
     return resultado
-
 
 
 problemas = ['3 + 855', '988 + 40']
 
 if __name__ == '__main__':
     print(arithmetic_arranger(problemas, True))
-
-
